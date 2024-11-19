f7d6249ba8ede8609f4f7afbf510f9e2b70da802
Wen-1019 <wenjingru@ufl.edu>
2024-11-19 10:40:08 -0500

    1119
    1

--- Assets/showmenu.cs
+++ Assets/showmenu.cs
@@ -5,14 +5,14 @@ using UnityEngine.InputSystem;
 
 public class showmenu : MonoBehaviour
 {
-    //public Transform head;
-    //public float spawnDistance = 1f;
+    //private Transform head;
+    //public float spawnDistance = 2f;
     public GameObject menu;
     public InputActionProperty showButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -21,7 +21,8 @@ public class showmenu : MonoBehaviour
         if (showButton.action.WasPressedThisFrame())
         {
             menu.SetActive (!menu.activeSelf);
-            //menu.transform.position = head.position + new Vector3(head.forward.x,0,head.forward.z).normalized * spawnDistance;
+            playerManager.GetInstance().FreezePlayer(!menu.activeSelf);
+
         }
         //menu.transform.LookAt(new Vector3(head.forward.x, menu.transform.position.y, head.forward.z));
         //menu.transform.forward *= -1;
